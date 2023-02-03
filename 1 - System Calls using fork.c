a74a77ac824f7fa30040b023ac5ef01075c4d344
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-02-03 17:35:15 +0530

    Update 1 - System Calls using fork.c

--- 1 - System Calls using fork.c
+++ 1 - System Calls using fork.c
@@ -1,23 +1,32 @@
 #include<stdio.h>
 #include<unistd.h>
 #include<sys/types.h>
-
 int main()
 {
-	pid_t p;
-	printf("before fork\n");
-	p=fork();
-	if(p==0)
-	{
-		printf("I am child having ID %d\n",getpid());
-		printf("My parent's ID is %d\n",getppid());
-	}
-	
-	else
-	{
-		printf("My child's ID is %d\n",p);
-		printf("I am parent having ID %d\n",getpid());
-	}
-	
-	printf("Common\n");
+pid_t fork(void);
+pid_t p;
+printf("before fork\n");
+p=fork();
+if(p==0)
+{
+printf("I am child having id %d\n",getpid());
+printf("My parent's id is %d\n",getpid());
 }
+else{
+printf("My child's id is %d\n",p);
+printf("I am parent having id %d\n",getpid());
+}
+printf("Common\n");
+return 0;
+}
+
+/* OUTPUT 
+
+
+before fork
+My child's id is 88973
+I am parent having id 88972
+Common
+I am child having id 88973
+My parent's id is 88973
+Common
