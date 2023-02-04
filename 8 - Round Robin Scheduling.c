57080c0e539ae8cdf62edb65f887fcc25c7370cd
Bandi Murali Chowdary <97547174+bandimuralichowdary@users.noreply.github.com>
2023-02-04 14:52:05 +0530

    Update 8 - Round Robin Scheduling.c

--- 8 - Round Robin Scheduling.c
+++ 8 - Round Robin Scheduling.c
@@ -1,12 +1,12 @@
 #include<stdio.h>  
-#include<conio.h>  
-int main()  
-{   
+  int main()  
+{  
     int i, NOP, sum=0,count=0, y, quant, wt=0, tat=0, at[10], bt[10], temp[10];  
     float avg_wt, avg_tat;  
     printf(" Total number of process in the system: ");  
     scanf("%d", &NOP);  
     y = NOP; 
+
 for(i=0; i<NOP; i++)  
 {  
 printf("\n Enter the Arrival and Burst time of the Process[%d]\n", i+1);  
@@ -14,14 +14,14 @@ printf(" Arrival time is: \t");
 scanf("%d", &at[i]);  
 printf(" \nBurst time is: \t");   
 scanf("%d", &bt[i]);  
-temp[i] = bt[i];  
-}    
+temp[i] = bt[i]; 
+}   
 printf("Enter the Time Quantum for the process: \t");  
-scanf("%d", &quant);   
+scanf("%d", &quant);  
 printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");  
 for(sum=0, i = 0; y!=0; )  
 {  
-if(temp[i] <= quant && temp[i] > 0)  
+if(temp[i] <= quant && temp[i] > 0)
 {  
     sum = sum + temp[i];  
     temp[i] = 0;  
@@ -34,8 +34,8 @@ if(temp[i] <= quant && temp[i] > 0)
     }  
     if(temp[i]==0 && count==1)  
     {  
-        y--;   
-        printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
+        y--; 
+        printf("\nProcess No[%d] \t\t %d\t\t\t %d\t\t %d", i+1, bt[i], sum-at[i], sum-at[i]-bt[i]);  
         wt = wt+sum-at[i]-bt[i];  
         tat = tat+sum-at[i];  
         count =0;     
@@ -52,10 +52,34 @@ if(temp[i] <= quant && temp[i] > 0)
     {  
         i=0;  
     }  
-}   
+}  
 avg_wt = wt * 1.0/NOP;  
 avg_tat = tat * 1.0/NOP;  
 printf("\n Average Turn Around Time: \t%f", avg_wt);  
 printf("\n Average Waiting Time: \t%f", avg_tat);  
-getch();  
-}
+return 0; 
+  }
+   
+
+
+/* OUTPUT 
+ 
+ Total number of process in the system: 2
+
+ Enter the Arrival and Burst time of the Process[1]
+ Arrival time is:       2
+
+Burst time is:  7
+
+ Enter the Arrival and Burst time of the Process[2]
+ Arrival time is:       1
+
+Burst time is:  10
+Enter the Time Quantum for the process:         3
+
+ Process No              Burst Time              TAT             Waiting Time
+Process No[1]            7                       11              4
+Process No[2]            10                      16              6
+ Average Turn Around Time:      5.000000
+ Average Waiting Time:  13.500000
+--------------------------------
